5a4fb473fb7b3bd372fa9e4e5d15f261ac8ff63a
theDAOS <sanjusanthosh46@gmail.com>
2025-05-30 23:31:28 +0530

    feat: add react-type-animation for enhanced text effects in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { TypeAnimation } from 'react-type-animation';
+
 function Home() {
     return (
         <div className="h-screen flex justify-center items-center border text-5xl font-minecraftia">
-            <span>front-end. web (developer)_</span>
+            {/* <span>front-end. web (developer)_</span> */}
+            <TypeAnimation
+                sequence={[
+                    // "i'm sanju santhosh",
+                    1000,
+                    'front-end. web (developer)',
+                ]}
+                wrapper="span"
+                className='cursor'
+                cursor={false}
+            />
         </div>
     )
 }
