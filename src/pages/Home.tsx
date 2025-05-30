03023fadf0dbf0e0c3600fba204041ddd2a4a624
theDAOS <sanjusanthosh46@gmail.com>
2025-05-30 23:14:15 +0530

    Implement code changes to enhance functionality and improve performance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <div className="h-screen flex justify-center items-center border text-5xl">
+        <div className="h-screen flex justify-center items-center border text-5xl font-minecraftia">
             <span>front-end. web (developer)_</span>
         </div>
     )
